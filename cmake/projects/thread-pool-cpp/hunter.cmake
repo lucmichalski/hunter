66e2c4c2473932131f1da4f62d5e43247dad539e
David Hirvonen <dhirvonen@elucideye.com>
2016-06-12 13:42:19 -0400

    add thread-pool-cpp

---
+++ cmake/projects/thread-pool-cpp/hunter.cmake
@@ -0,0 +1,24 @@
+# Copyright (c) 2016, Ruslan Baratov
+# All rights reserved.
+
+# !!! DO NOT PLACE HEADER GUARDS HERE !!!
+
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_add_version(
+    PACKAGE_NAME
+    thread-pool-cpp
+    VERSION
+    "1.0.0"
+    URL
+    https://github.com/headupinclouds/thread-pool-cpp/archive/v1.0.0.tar.gz
+    SHA1
+    8c657d3b3f494761838cc0c42e83ede82ff31525    
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+hunter_cacheable(thread-pool-cpp)
+hunter_download(PACKAGE_NAME thread-pool-cpp)
